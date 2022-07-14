3312954a6fa9c0a200e41e328afdf00c3e69015a
Ibrahemdh7 <ibrahemdh7@gmail.com>
2022-07-14 21:07:02 +0300

    Add project files.

---
+++ TestOppNameSpace/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOppNameSpace
+{
+
+    class Employee
+    {
+        public int id;
+        public string name;
+        public string address;
+        public string phoneNo;
+        public double salary;
+        public const double tax = 0.15;
+
+        public double getNetSalary()
+        {
+            double netSal = salary - (salary * tax);
+            return netSal;
+        }
+
+        public string getEmployeeData()
+        {
+            string alldata = "ID: " + id + "\n";
+            alldata += "Name: " + name + "\n";
+            alldata += "Address: " + address + "\n";
+            alldata += "Phone:" + phoneNo + "\n";
+            alldata += "Salary: " + salary + "\n";
+            alldata += "Net Salary: " + getNetSalary() + "\n";
+            return alldata;
+        }
+
+        public void printEmployeeData()
+        {
+            Console.WriteLine(getEmployeeData());
+        }
+    }
+  
+}
---
+++ TestOppNameSpace/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO ;
+
+
+
+
+namespace TestOppNameSpace
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Id");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Your City");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter Your Number Phone");
+            string phone = Console.ReadLine();
+            Console.WriteLine("==============================================");
+
+            Employee e1 = new Employee();
+            e1.id =  id;
+            e1.name = name;
+            e1.address = city;
+            e1.phoneNo = phone;
+            e1.salary = 15000;
+            e1.printEmployeeData();
+            Console.WriteLine("==============================================");
+            Employee e2 = new Employee();
+            e2.id = 12345678;
+            e2.name = "Dempsy";
+            e2.address = "Origins";
+            e2.phoneNo = "0551626451";
+            e2.salary = 20000;
+            e2.printEmployeeData();
+            Console.WriteLine("==============================================");
+
+            Console.WriteLine("Enter Your Id");
+            int id2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Name");
+            string name2 = Console.ReadLine();
+            Console.WriteLine("Enter Your City");
+            string city2 = Console.ReadLine();
+            Console.WriteLine("Enter Your Number Phone");
+            string phone2 = Console.ReadLine();
+            Console.WriteLine("==============================================");
+            Employee e3 = new Employee();
+            e3.id = id2;
+            e3.name = name2;
+            e3.address = city2;
+            e3.phoneNo = phone2;
+            e3.salary = 20000;
+            e3.printEmployeeData();
+
+        }
+    }
+}
